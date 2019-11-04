3f951d2365757552aa81774cb56c36be7e3c2670
tony <tony92151@gmail.com>
2019-11-04 20:10:36 +0800

    extrinsic success

--- hw1/hw1-1/main.py
+++ hw1/hw1-1/main.py
@@ -48,6 +48,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.RMS = None
         self.camera_matrix = []
         self.distortion_coefficients = []
+        self.rotation_matrix = []
 
         # pre-read imgs in memory as array
         self.read_img()
@@ -69,6 +70,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.bt_intrinsic.clicked.connect(self.on_bt_intrinsic_click)
         self.bt_distortion.clicked.connect(self.on_bt_distortion_click)
+        self.bt_extrinsic.clicked.connect(self.on_bt_extrinsic_click)
 
     def on_bt_find_corners_click(self):
         find_corner_img = self.find_imgs_corner()
@@ -98,6 +100,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.camera_calibration()
         print ("Distortion matrix:\n", self.distortion_coefficients)
 
+    def on_bt_extrinsic_click(self):
+        if len(self.distortion_coefficients) == 0 :
+            self.camera_calibration()
+        #print(self.selected_img[:-4])
+        idx = self.selected_img[:-4]
+        print ("Extrinsic matrix of img "+idx+" :\n", self.rotation_matrix[int(idx)-1])
+
     def comboBox_onChanged(self,text):
         self.selected_img = text
 
@@ -115,10 +124,45 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def camera_calibration(self):
         if len(self.objpoints) == 0 :
             self.find_imgs_corner()
-        ret, mtx, dist, _, _ = cv2.calibrateCamera(self.objpoints, self.imgpoints, self.gray_imgs[0].shape[::-1],None,None)
+        ret, mtx, dist, rvecs, tvecs  = cv2.calibrateCamera(self.objpoints, self.imgpoints, self.gray_imgs[0].shape[::-1],None,None)
         self.RMS = ret
         self.camera_matrix = mtx
         self.distortion_coefficients = dist.ravel()
+        """
+        # rvecs is rotation vector, not the rotation matrix
+        # tvecs is translation vector
+        Vr = np.array(rvecs)
+        Tr = np.array(tvecs)
+        extrinsics = np.concatenate((Vr, Tr), axis=1).reshape(-1,6)
+        """
+        
+        #print(np.array(rvecs).shape)
+        # rt,_ = cv2.Rodrigues(np.array(rvecs)[0])
+
+        
+
+        # Tr = np.array(tvecs)
+        # #rtt = np.append(rt.T, Tr[0])
+
+        # ex = np.concatenate((rt, Tr[0]),axis = 1)
+
+        # print("RT : \n",rt.T)
+        # print("R : \n",rt)
+        # #print("RTT : \n",rtt)
+        # print("TR : \n",Tr[0].T[0])
+        # print("ex : \n",ex)
+        #print(dist)
+        #print(np.array(rt).shape)
+        Vr = np.array(rvecs)
+        Tr = np.array(tvecs)
+        for i in range(len(Tr)):
+            rt,_ = cv2.Rodrigues(Vr[i])
+
+            ex = np.concatenate((rt, Tr[i]),axis = 1)
+            self.rotation_matrix.append(ex)
+        
+        # extrinsics = np.concatenate((Vr, Tr), axis=1).reshape(-1,6)
+        # print(extrinsics)
 
     def find_imgs_corner(self):
 
